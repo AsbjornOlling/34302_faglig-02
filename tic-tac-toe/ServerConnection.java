66ef7f2e6ed9bd1001b0028081edf42d9021a3be
DaniliJog <Danili.jog@gmail.com>
2017-11-06 16:51:05 +0100

    Get first 3 lines from server

--- tic-tac-toe/ServerConnection.java
+++ tic-tac-toe/ServerConnection.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.net.Socket;
+
 /* Handles connection to server;
  *
  * Contains methods that will send
@@ -9,37 +12,46 @@
  */
 
 public class ServerConnection {
-	
+
 	// Flags
-	private static InetAddress host;  // The host adress
-	private static final int PORT;  // The host port
-	public String startingPlayer;  // Who starts 
-	public String boardState;  // What does the board look like now
-	public String winState;  // The line about win/lose or keep going
-	
+	private static InetAddress host; // The host adress
+	private static final int PORT; // The host port
+	public String startingPlayer; // Who starts
+	public String boardState; // What does the board look like now
+	public String gameState; // The line about win/lose or keep going
+	public boolean serverIsActive;
+
 	// The constructor handling the server connection
-	public ServerConnect (String IP, int port) {
+	public ServerConnect(String IP, int port) {
 
 		// Initializing our flags
 		PORT = port;
 		host = IP;
-		
+
 		// Run the method to access the server
 		accessServer();
 	}
-	
+
 	public static void accessServer() {
-		
+
 		// Variable for link
 		Socket link = null;
-		
+
 		// Error handling for link
-		try
-		{
-			link = new Socket(host,PORT);
+		try {
+			link = new Socket(host, PORT);
+
+			serverIsActive = true;
+			BufferedReader response = new BufferedReader(link.getInputStream());
+
+			// First round
+			startingPlayer = response.readLine();
+			boardState = response.readLine();
+			gameState = response.readLine();
+		} catch (IOException ioEx) {
+			ioEx.printStackTrace();
 		}
-		
-		
+
 	}
 
 }
