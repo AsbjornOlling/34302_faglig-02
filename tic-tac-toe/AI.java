890078f204b89bbb96cdb67b4dd90eb568f4e981
AsbjornOlling <asbjornolling@gmail.com>
2017-11-07 01:53:17 +0100

    Shits broken and I'm going to bed

--- tic-tac-toe/AI.java
+++ tic-tac-toe/AI.java
@@ -4,16 +4,45 @@
 
 public class AI {
 	private static final int MAX_DEPTH = 3;
+	private static final char PLAYER = 'X';
+	private static final char OPPONENT = 'O';
+
 	public static void main(String[] args) {
+		char[] aBoard = {'.','.','o',
+										 '.','.','o',
+										 'o','o','.'};
+		System.out.println(makeMove(aBoard)+"");
 	}
 
-	public static int evaluateBoard(char[] passedBoard, char player, int depth) {
-		int returnValue = 0;
-		char otherPlayer = null;
+	public static int makeMove(char[] board) {
+		int bestMoveScore = -1000; // easy to beat
+		int bestMove = 999; // obviousy wrong
+		for (int i = 0; i < board.length; i++) {
+			if (board[i] == '.') {
+				// make modified board
+				char[] newBoard = board;
+				newBoard[i] = PLAYER;
 
-		if ( player == 'X' ) {
+				// check for win and run recursive method
+				int thisScore = 0;
+				if (checkForWin(board,PLAYER)) {
+					thisScore += 10;
+				}
+				if ( thisScore + evaluateBoard(newBoard,OPPONENT,0) > bestMoveScore){
+					bestMove = i;
+				}
+			}		
+		} // loop
+		return bestMove;
+	} // makeMove
+
+	public static int evaluateBoard(char[] passedBoard, char currentPlayer, int depth) {
+		int returnValue = 0;
+		char otherPlayer = ' ';
+
+		if ( currentPlayer == 'X' ) {
 			otherPlayer = 'O';
-		} else if ( player == 'O' ) {
+		} else if ( currentPlayer == 'O' ) {
 			otherPlayer = 'X';
 		}
 
@@ -22,14 +51,14 @@ public class AI {
 
 				// make new modified board
 				char[] newBoard = passedBoard;
-				newBoard[i] = player;
+				newBoard[i] = currentPlayer;
 
 				// check for wins
-				if ( checkForWin(newBoard,player) ) {
+				if ( checkForWin(newBoard,PLAYER) ) {
 					returnValue += 10;
-				} else if ( checkForWin(newBoard,otherPlayer) ) {
+				} else if ( checkForWin(newBoard,OPPONENT) ) {
 					returnValue -= 10;
-				} else if ( depth >= MAX_DEPTH ){
+				} else if ( depth < MAX_DEPTH ){
 					depth++;
 					returnValue += evaluateBoard(newBoard,otherPlayer,depth);
 				}
