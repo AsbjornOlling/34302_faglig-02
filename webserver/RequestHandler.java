7e20dd18eb6df61a30342289de3aede4cd91fc29
DaniliJog <Danili.jog@gmail.com>
2017-11-13 15:18:06 +0100

    printer URI for test

--- webserver/RequestHandler.java
+++ webserver/RequestHandler.java
@@ -4,3 +4,11 @@
  * and returns (some kind of) outputtable object to ClientConnection.
  * 
  */
+
+  //Fields
+  String status;
+
+ public handler (String URI) {
+	 String[] split = URI.split("\\s+");
+	 System.out.println(split[1]);
+ }
