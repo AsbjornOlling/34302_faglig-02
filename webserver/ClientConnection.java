2f7dc1f917d5fb5b48d24570701a286f1a2fded5
AsbjornOlling <asbjornolling@gmail.com>
2017-11-14 20:30:24 +0100

    sendResponse is (somewhat) functional

--- webserver/ClientConnection.java
+++ webserver/ClientConnection.java
@@ -17,6 +17,7 @@ import java.net.ServerSocket;
 
 public class ClientConnection {
 	boolean serverActive = true; // what even do with this
+	final boolean DEBUG = true;
 	// socket fields
 	ServerSocket serverSocket;
 	Socket clientSocket;
@@ -24,10 +25,15 @@ public class ClientConnection {
 
 	// TEMPORARY CODEBLOCK FOR DEBUGGIN
 	public static void main(String[] args){
+		// make connection, get request
 		ClientConnection connection = new ClientConnection(8080);
 		ArrayList<String> request = connection.getNextRequest();
 		for (String line : request) System.out.println(line);
-		connection.sendResponse("shiiiiet".getBytes());
+
+		// make bogus response object
+		Response res = new Response(200, "html", "hello world".getBytes());
+		// send it back
+		connection.sendResponse(res);
 	} // main */
 
 
@@ -65,21 +71,13 @@ public class ClientConnection {
 			System.out.println("ERROR: Could not read line from InputStream");
 		}
 
-
-		/*
-		try { // close the client connection
-			clientSocket.close();
-		} catch (IOException ioEx) {
-			System.out.println("ERROR: Could not close connection to client.");
-		} //*/
-
 		return request;
 	} //getNextRequest
 
 
 	// send a bytearray back
 	// TODO write headers
-	public void sendResponse(byte[] data) {
+	public void sendResponse(Response response) {
 		// open output stream
 		BufferedOutputStream output = null;
 		try { 
@@ -88,14 +86,34 @@ public class ClientConnection {
 			System.out.println("ERROR: Could not open outputstream to browser.");
 		}
 
-		// write byte[] data to output
+		// write some shit to browser
 		try { 
-			output.write(data);
+			output.write( response.HTTP_STATUS.getBytes() );
+			if ( DEBUG ) System.out.println(response.HTTP_STATUS);
+
+			output.write( response.HTTP_DATE.getBytes() );
+			if ( DEBUG ) System.out.println(response.HTTP_DATE);
+
+			output.write( response.HTTP_CONTENTTYPE.getBytes() );
+			if ( DEBUG ) System.out.println(response.HTTP_CONTENTTYPE);
+
+			output.write( response.HTTP_CONTENTLENGTH.getBytes() );
+			if ( DEBUG ) System.out.println(response.HTTP_CONTENTLENGTH);
+			
+			output.write( "\r\n".getBytes() );
+			if ( DEBUG ) System.out.println();
+
+			output.write( response.FILE_CONTENTS );
+			if ( DEBUG ) System.out.println( new String(response.FILE_CONTENTS) );
 		} catch (IOException ioEx) {
 			System.out.println("ERROR: Could not write data to outputstream.");
 		}
 
-		// TODO close socket here
+		try { // close the client connection
+			clientSocket.close();
+		} catch (IOException ioEx) {
+			System.out.println("ERROR: Could not close connection to client.");
+		} //*/
 	} // sendResponse
 
 } // class
