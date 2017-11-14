c86761227fbfa014a05152eaece6c20a8eb359b7
AsbjornOlling <asbjornolling@gmail.com>
2017-11-14 17:57:24 +0100

    response objects now have date

--- webserver/Response.java
+++ webserver/Response.java
@@ -4,18 +4,32 @@
  * instantiated by RequestHandler
  */
 
+import java.util.*;
+import java.text.SimpleDateFormat;
+
 public class Response {
+	public String DATE_CREATED;
 	public final int HTTP_STATUS;
 	public final String FILETYPE;
 	public final int FILESIZE;
-	public byte[] FILE_CONTENTS;
+	public byte[] FILE_CONTENTS; // can this be final?
 
 
-	public Response(int HTTP_STATUS, int FILETYPE, int FILESIZE, byte[] FILE_CONTENTS) {
+	public Response(int HTTP_STATUS, String FILETYPE, int FILESIZE, byte[] FILE_CONTENTS) {
+		this.DATE_CREATED = date();
 		this.HTTP_STATUS = HTTP_STATUS;
 		this.FILETYPE = FILETYPE;
 		this.FILESIZE = FILESIZE;
 		this.FILE_CONTENTS = FILE_CONTENTS;
 	} // constructor
+
+	private String date() {
+		Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat dateFormat = new SimpleDateFormat(
+        "EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+    dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+    return dateFormat.format(calendar.getTime());
+	}
+
 } // class
  
