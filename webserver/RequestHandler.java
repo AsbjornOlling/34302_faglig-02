79ecc8db524a40fa55b2704a74c4b22ee8b9d4a9
DaniliJog <Danili.jog@gmail.com>
2017-11-14 17:29:28 +0100

    Status and ext is now available through fields

--- webserver/RequestHandler.java
+++ webserver/RequestHandler.java
@@ -10,25 +10,28 @@ import java.io.*;
 public class RequestHandler {
 		
   	//Fields
-  	String status;
+  	int status;
+  	String ext;
 
   	public static void Handler (String FirstLine) {
   		
   		//Variables
   		String uri;
   		
-  		//Metod handling
+  		// Gets the URI for the file
   		String[] split = FirstLine.split("\\s+");
   		uri = split[1];
   		
+  		// Gets the file extension for the file 
   		String[] dotArray = uri.split("[.]+");
-  		String ext = dotArray[dotArray.length - 1];
+  		ext = dotArray[dotArray.length - 1];
   		System.out.println(ext);
   		try {
   			BufferedReader fil = new BufferedReader ( new FileReader ("content" + uri) );
-  			System.out.println(uri);
+  			status = 200;
   		} catch (FileNotFoundException e){
   		    System.out.println(e);
+  		    status = 404;
   		}
   	}
   	
